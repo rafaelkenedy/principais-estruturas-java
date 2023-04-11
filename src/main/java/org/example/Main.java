072a43e18288262063517064f24d93a0d1d055f9
rafael <k.rafaelalves@gmail.com>
2023-04-11 20:13:51 -0300

    stack, queue, list, set(hashset), map(hashMap)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -42,10 +42,52 @@ public class Main {
 
         System.out.println(listaDeCarros);
 
+        Set<Carro> hashSetCarros = new HashSet<>();
 
+        hashSetCarros.add(new Carro("Ford"));
+        hashSetCarros.add(new Carro("Fiat"));
+        hashSetCarros.add(new Carro("Peugeot"));
+        hashSetCarros.add(new Carro("Zip"));
 
+        System.out.println(hashSetCarros);
 
+        Set<Carro> treeSetCarros = new TreeSet<>();
 
+        treeSetCarros.add(new Carro("Ford"));
+        treeSetCarros.add(new Carro("Fiat"));
+        treeSetCarros.add(new Carro("Peugeot"));
+        treeSetCarros.add(new Carro("Zip"));
 
+        System.out.println(treeSetCarros);
+
+        Map<String, String> aluno = new HashMap<>();
+
+        aluno.put("Nome", "João");
+        aluno.put("Idade", "17");
+        aluno.put("Média", "8.5");
+        aluno.put("Turma", "3a");
+
+        Map<String, String> aluno2 = new HashMap<>();
+
+        aluno2.put("Nome", "Maria");
+        aluno2.put("Idade", "18");
+        aluno2.put("Média", "8.9");
+        aluno2.put("Turma", "3b");
+
+        System.out.println();
+
+        System.out.println(aluno);
+        System.out.println(aluno.keySet());
+        System.out.println(aluno.values());
+
+        List<Map<String, String>> listaAlunos = new ArrayList<>();
+
+        listaAlunos.add(aluno);
+        listaAlunos.add(aluno2);
+
+        System.out.println(listaAlunos);
+
+        System.out.println(aluno.containsKey("Nome"));
+        System.out.println(aluno.containsKey("Nome2"));
     }
 }
